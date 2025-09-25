1fec2b989678d5dc1652db840ccf673f87cead23
Juan Camilo Salazar Serna <jcss1462@gmail.com>
2025-09-25 17:13:41 -0400

    Ajustes visuales logo header

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -5,7 +5,7 @@ export default function Header() {
     <header className="w-full min-h-[80px]  h-[10vh] shadow-sm bg-[var(--color-brand-dark)] border-b flex items-center justify-between px-6">
       {/* Sección izquierda: logo + texto */}
       <div className="flex items-center gap-4">
-        <img src="/semilloArtificialLogo.png" alt="Logo Izquierdo" className="h-15 w-auto" />
+        <img src="/semilloArtificialLogo.png" alt="Logo Izquierdo" className="h-17 w-auto" />
         <Link
           to="/"
           className="text-3xl font-bold text-brand-white hover:text-brand-accent transition-colors"
@@ -15,7 +15,7 @@ export default function Header() {
       </div>
 
       {/* Sección derecha: logo derecho */}
-      <img src="/openLogoWhite.png" alt="Logo Derecho" className="h-8 w-auto" />
+      <img src="/openLogoWhite.png" alt="Logo Derecho" className="h-12 w-auto" />
     </header>
   );
 }
